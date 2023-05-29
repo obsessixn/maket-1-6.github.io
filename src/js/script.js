ac410a9e64dda28e1cb0b449da125b5573713a35
Daniil <butmanovich@gmail>
2023-05-29 14:43:28 +0200

    last commit

---
+++ src/js/script.js
@@ -0,0 +1,124 @@
+let mediaQuery = window.matchMedia('(max-width: 1366px)')
+let mediaQueryTwo = window.matchMedia('(min-width: 1366px)')
+let mediaQueryThree = window.matchMedia('(max-width: 768px)')
+let menuBlur = document.querySelector('.menu-blur')
+
+//swiper-----------------------------------------------------//
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  breakpoints: {
+    768: {
+      allowTouchMove: false
+    }
+  }
+})
+
+//menu--------------------------------------------------------//
+let menu = document.querySelector('.menu')
+let headerMenu = document.querySelector('.header__menu')
+let closeHeader = document.querySelector('.menu-header__close')
+
+headerMenu.addEventListener('click', function () {
+  menu.removeAttribute('style')
+  menu.classList.add('menu--showed')
+  menuBlur.classList.add('menu-blur--showed')
+})
+
+menuBlur.addEventListener('click', function () {
+  menu.classList.remove('menu--showed')
+  call.classList.remove('call--showed')
+  menuBlur.classList.remove('menu-blur--showed')
+  feedback.classList.remove('feedback--showed')
+})
+
+closeHeader.addEventListener('click', function () {
+  menu.removeAttribute('style')
+  menu.classList.remove('menu--showed')
+  menuBlur.classList.remove('menu-blur--showed')
+})
+
+//call--------------------------------------------------------//
+let headerCall = document.querySelector('.header__call')
+let menuCall = document.querySelector('.menu-footer__call')
+let call = document.querySelector('.call')
+let callClose = document.querySelector('.call__close')
+
+headerCall.addEventListener('click', function () {
+  call.classList.add('call--showed')
+  menuBlur.classList.add('menu-blur--showed')
+  feedback.classList.remove('feedback--showed')
+})
+menuCall.addEventListener('click', function () {
+  call.classList.add('call--showed')
+  menuBlur.classList.add('menu-blur--showed')
+  menu.classList.remove('menu--showed')
+  feedback.classList.remove('feedback--showed')
+})
+callClose.addEventListener('click', function () {
+  menuBlur.classList.remove('menu-blur--showed')
+  call.classList.remove('call--showed')
+})
+//feedback--------------------------------------------------------//
+let headerFeedback = document.querySelector('.header__chat')
+let menuFeedback = document.querySelector('.menu-footer__chat')
+let feedback = document.querySelector('.feedback')
+let feedbackClose = document.querySelector('.feedback__close')
+headerFeedback.addEventListener('click', function () {
+  feedback.classList.add('feedback--showed')
+  menuBlur.classList.add('menu-blur--showed')
+  call.classList.remove('call--showed')
+})
+menuFeedback.addEventListener('click', function () {
+  feedback.classList.add('feedback--showed')
+  menuBlur.classList.add('menu-blur--showed')
+  menu.classList.remove('menu--showed')
+  call.classList.remove('call--showed')
+})
+feedbackClose.addEventListener('click', function () {
+  menuBlur.classList.remove('menu-blur--showed')
+  feedback.classList.remove('feedback--showed')
+})
+//read-more----------------------------------------------------//
+let readMore = document.querySelectorAll('.read-more')
+let brandsMain = document.querySelector('.brands__swiper-wrapper')
+let techMain = document.querySelector('.tech__swiper-wrapper')
+let pageText = document.querySelector('.page__text')
+
+readMore.forEach((button) => {
+  button.addEventListener('click', function () {
+    showMore(button)
+  })
+})
+
+function showMore(button) {
+  if (button.textContent === 'Показать все') {
+    brandsMain.style.height = `${brandsMain.scrollHeight}px`
+    button.textContent = 'Скрыть'
+    button.classList.toggle('read-more--arrow-up')
+  } else if (button.textContent === 'Показать все ') {
+    techMain.style.height = `${techMain.scrollHeight}px`
+    button.textContent = 'Скрыть '
+    button.classList.toggle('read-more--arrow-up')
+  } else if (button.textContent === 'Читать далее') {
+    pageText.style.height = `${pageText.scrollHeight}px`
+    button.textContent = 'Скрыть текст'
+    button.classList.toggle('read-more--arrow-up')
+  } else if (button.textContent === 'Скрыть') {
+    button.textContent = 'Показать все'
+    brandsMain.style.height = 160 + 'px'
+    button.classList.toggle('read-more--arrow-up')
+  } else if (button.textContent === 'Скрыть текст') {
+    button.textContent = 'Читать далее'
+    pageText.style.height = 90 + 'px'
+    button.classList.toggle('read-more--arrow-up')
+  } else if (button.textContent === 'Скрыть ') {
+    button.textContent = 'Показать все '
+    techMain.style.height = 160 + 'px'
+    button.classList.toggle('read-more--arrow-up')
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,104 @@
 const path = require('path');
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode ==='development'
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-  // Входной файл
-  entry: [
-    './src/js/index.js'
-  ],
-
-  // Выходной файл
-  output: {
-    filename: './js/bundle.js'
+  mode,
+  target,
+  devtool,
+  devServer: {
+    open: true,
+    hot: true,
   },
-
-  // Source maps для удобства отладки
-  devtool: "source-map",
-
+  stats: {
+    children: true
+  },
+  entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename:'[name].index.[contenthash].js',
+    assetModuleFilename: 'assets/[name][ext]'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new MiniCssExtractPlugin ({
+      filename: '[name].index.[contenthash].css'
+    })
+  ],
   module: {
     rules: [
-      // Транспилируем js с babel
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src/js'),
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [devMode ? "style-loader" : MiniCssExtractPlugin.loader, 
+        "css-loader",
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [require('postcss-preset-env')],
+            }
+          }
+        },
+        "sass-loader"],
+      },
+      {
+        test: /\.woff2?$/i,
+        type: 'asset/resource',
+        generator: {
+          filename:'fonts/[name][ext]'
+        }
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ],
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/i,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
           }
         }
-      },
-
-      // Компилируем SCSS в CSS
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader, // Extract css to separate file
-          'css-loader', // translates CSS into CommonJS
-          'postcss-loader', // parse CSS and add vendor prefixes to CSS rules
-          'sass-loader', // compiles Sass to CSS, using Node Sass by default
-        ],
-      },
-
-      // Подключаем шрифты из css
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader?name=./fonts/[name].[ext]'
-          },
-        ]
-      },
-
-      // Подключаем картинки из css
-      {
-        test: /\.(svg|png|jpg|jpeg|webp)$/,
-        use: [
-          {
-            loader: 'file-loader?name=./static/[name].[ext]'
-          },
-        ]
-      },
-    ],
-  },
-  plugins: [
-    // Подключаем файл html, стили и скрипты встроятся автоматически
-    new HtmlWebpackPlugin({
-      title: 'Webpack 4 Starter',
-      template: './src/index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: false,
       }
-    }),
-
-    // Кладем стили в отдельный файлик
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-
-    // Копируем картинки
-    new CopyWebpackPlugin([
-      {
-        from: './src/img',
-        to: 'img',
-      },
-    ])
-  ],
-};
+    ]
+  }
+}
